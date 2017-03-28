07f48c9c7bee2fb25211dcffe2c4cc9a42c21568
Ankit Goel <ankit.goel@mnsu.edu>
2017-03-28 06:40:17 +0000

    test

--- db/migrate/20170328054014_create_users.rb
+++ db/migrate/20170328054014_create_users.rb
@@ -3,9 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :provider, null: false
       t.string :uid, null: false
-      add_index :users, :provider
-      add_index :users, :uid
-      add_index :users, [:provider, :uid], unique: true
+      
       t.string :name
       t.string :location
       t.string :image_url
@@ -13,5 +11,8 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_index :users, :provider
+    add_index :users, :uid
+    add_index :users, [:provider, :uid], unique: true
   end
 end
