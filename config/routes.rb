e4cf927c79055b593844428292184a61c0cc71e8
Ankit Goel <ankit.goel@mnsu.edu>
2017-03-28 21:33:49 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/redirect', to: 'example#redirect', as: 'redirect'
   get '/callback', to: 'example#callback', as: 'callback'
   get '/calendars', to: 'example#calendars', as: 'calendars'
+  
   get 'sessions/create'
   delete '/logout', to: 'sessions#destroy'
 
