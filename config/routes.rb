b60b0832644b3aa64ebca90fa064d756b3f97d50
Ankit Goel <ankit.goel@mnsu.edu>
2017-04-11 03:39:05 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get '/events/:calendar_id', to: 'example#events', as: 'events', calendar_id: /[^\/]+/
   post '/events/:calendar_id', to: 'example#new_event', as: 'new_event', calendar_id: /[^\/]+/
-  #get 'events/create', to: 'example#create_event', as: 'create_event'
+  get '/create', to: 'example#create_event', as: 'create_event'
   
-  get '/events/create', to: :create_event, controller: 'example', as: 'create_event'
+  #get '/events/create', to: :create_event, controller: 'example', as: 'create_event'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
