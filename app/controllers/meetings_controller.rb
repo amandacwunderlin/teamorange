8fce2b72935eca0f4862f1a355731e06bb6bf001
Ankit Goel <ankit.goel@mnsu.edu>
2017-04-11 07:36:29 +0000

    meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -5,10 +5,34 @@ class MeetingsController < ApplicationController
     end
     
     def create
-        @meeting = Meeting.new(params.require(:meeting).permit(:startdate, :enddate, :summary))
-        if @meeting.save
-            redirect_to new_meeting_url
+      @meeting = Meeting.new(params.require(:meeting).permit(:startdate, :enddate, :summary))
+        if @meeting.save  
+            client = Signet::OAuth2::Client.new({
+                client_id: Rails.application.secrets.google_client_id,
+                client_secret: Rails.application.secrets.google_client_secret,
+                token_credential_uri: 'https://accounts.google.com/o/oauth2/token'
+            })
+          
+            client.update!(session[:authorization])
+          
+            service = Google::Apis::CalendarV3::CalendarService.new
+            service.authorization = client
+              
+            
+        
+            event = Google::Apis::CalendarV3::Event.new({
+            start: Google::Apis::CalendarV3::EventDateTime.new(date: @meeting.startdate),
+            end: Google::Apis::CalendarV3::EventDateTime.new(date: @meeting.enddate),
+                summary: @meeting.summary
+              })
+              
+              
+            result = service.insert_event('primary', event)
+            redirect_to root_url
+                
         end
+    
+        
     end
     
     
